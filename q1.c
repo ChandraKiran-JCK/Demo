6369de5a3de295bf533d4fb26bc76a534a965368
ChandraKiran-JCK <115851999+ChandraKiran-JCK@users.noreply.github.com>
2022-10-22 10:16:29 +0530

    1st program

---
+++ q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Enter the side of the square : \n");
+    int side ;
+    scanf("%d" , &side);
+
+    printf("Area of the square : %d\n" , (side*side));
+
+    return 0 ;
+}
